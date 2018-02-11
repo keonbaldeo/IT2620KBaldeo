e7bf47b4fc8cea9b0371eb529be068d07fd7263a
keonbaldeo <S00977574@acad.tri-c.edu>
2018-02-11 10:20:19 -0500

    Joke and Punchline

---
+++ Lab02-JokeandPunchline/Lab02-JokeandPunchline/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "What does the bartender say to the neutron?"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text = "You have no charge!"
+    End Sub
+End Class
