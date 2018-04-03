752be4664720cbf00d2cb0907bc596e9d1730d44
keonbaldeo <S00977574@acad.tri-c.edu>
2018-04-03 11:00:30 -0400

    Lab 6

---
+++ Lab06DistanceCalculator/Lab06DistanceCalculator/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim speed As Decimal
+        Dim time As Decimal
+        Dim distance As Decimal
+        Dim sumtotal As Decimal
+        speed = InputBox("In mph", "Input Speed")
+        time = InputBox("In hours", "Time")
+
+        distance = speed * time
+        ListBox1.Items.Add("Vehicle Speed" & " " & speed & " " & "MPH")
+        ListBox1.Items.Add("Time Traveled" & " " & time & " " & "Hours")
+
+        ListBox1.Items.Add("Hours" & "   " & "Distance Traveled")
+        ListBox1.Items.Add("_____________________________")
+
+        For i = 1 To time
+            sumtotal = sumtotal + speed
+            ListBox1.Items.Add(i & "            " & sumtotal)
+
+        Next
+
+        ListBox1.Items.Add("Total Distance" & " " & distance)
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
---
+++ Lab06SumNumbers/Lab06SumNumbers/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim userInput As String
+        Dim intcount As Integer
+        Dim sumcount As Integer
+
+        Try
+            userInput = InputBox("Enter a value", "Provide a value", "10")
+            If (Integer.TryParse(userInput, intcount) = False) Then
+                MessageBox.Show("Donot enter a string. Enter an integer")
+            Else
+                For i = 1 To intcount
+                    sumcount = sumcount + i
+                Next
+                MessageBox.Show(sumcount)
+            End If
+        Catch ex As Exception
+                MessageBox.Show("Error in your code")
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
+
