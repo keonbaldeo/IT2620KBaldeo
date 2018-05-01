d03e95d785d2acf113a2cbf54aaa09e16f484976
keonbaldeo <S00977574@acad.tri-c.edu>
2018-05-01 11:19:55 -0400

    fixed

--- Lab06RetailCalculator/Lab06RetailCalculator/Form1.vb
+++ Lab06RetailCalculator/Lab06RetailCalculator/Form1.vb
@@ -2,14 +2,20 @@
 
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim wholesale As Integer
-        Dim Percent As Decimal
+        Dim wholesale As String
+        Dim Percent As String
+
+        Dim Retail As Integer
 
         Try
 
             wholesale = TextBox1.Text
-            Percent = (TextBox2.Text / 100) * TextBox1.Text
-            Label4.Text = "$" & wholesale + Percent
+            Percent = TextBox2.Text
+
+            Retail = CalculateRetail(CInt(wholesale), CInt(Percent))
+
+            Label4.Text = "$" + Retail.ToString
+
 
             If (TextBox1.Text < 0) Then
                 MessageBox.Show("Positive Numbers Only")
@@ -23,6 +29,14 @@
 
 
     End Sub
+    Function CalculateRetail(ByVal wholesale As Integer, ByVal Percent As Integer)
+        Dim Retail As Decimal
+
+
+        Retail = wholesale + (Percent / 100 * wholesale)
+        Return Retail
+
+    End Function
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Me.Close()
