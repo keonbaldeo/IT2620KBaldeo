e648bb7ad569c45a062d40114d2c783f95f9ce6f
keonbaldeo <S00977574@acad.tri-c.edu>
2018-05-05 21:53:48 -0400

    Lab 8

--- Lab08TravelExpenses/Lab08TravelExpenses/Form1.Designer.vb
+++ Lab08TravelExpenses/Lab08TravelExpenses/Form1.Designer.vb
@@ -22,9 +22,319 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(172, 73)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 2
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(172, 99)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 3
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(172, 125)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 4
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(172, 151)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox6.TabIndex = 5
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(172, 177)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox7.TabIndex = 6
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(172, 203)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox8.TabIndex = 7
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(172, 229)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox9.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 76)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Days"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 102)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Airfare"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(8, 128)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Meals"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(8, 154)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Rental Fees"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(8, 180)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(65, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Miles Driven"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(8, 206)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(69, 13)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Parking Fees"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(8, 232)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(69, 13)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Taxi Charges"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(26, 335)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(121, 13)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "Total Expenses incurred"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(26, 358)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(128, 13)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Total Allowable Expenses"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(26, 381)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(66, 13)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "Amount Due"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(26, 404)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(77, 13)
+        Me.Label11.TabIndex = 19
+        Me.Label11.Text = "Amount Saved"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(92, 449)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label12
+        '
+        Me.Label12.Location = New System.Drawing.Point(169, 335)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(100, 23)
+        Me.Label12.TabIndex = 21
+        Me.Label12.Text = "Label12"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(8, 258)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(89, 13)
+        Me.Label13.TabIndex = 22
+        Me.Label13.Text = "Registration Fees"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(172, 255)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 23
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(8, 284)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(71, 13)
+        Me.Label14.TabIndex = 24
+        Me.Label14.Text = "Lodging Fees"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(172, 281)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 25
+        '
+        'Label15
+        '
+        Me.Label15.Location = New System.Drawing.Point(169, 358)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(100, 23)
+        Me.Label15.TabIndex = 26
+        Me.Label15.Text = "Label15"
+        '
+        'Label16
+        '
+        Me.Label16.Location = New System.Drawing.Point(169, 381)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(100, 23)
+        Me.Label16.TabIndex = 27
+        Me.Label16.Text = "Label16"
+        '
+        'Label17
+        '
+        Me.Label17.Location = New System.Drawing.Point(169, 404)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(100, 23)
+        Me.Label17.TabIndex = 28
+        Me.Label17.Text = "Label17"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
+        Me.ClientSize = New System.Drawing.Size(284, 500)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Name = "Form1"
+        Me.Text = "Travel Expenses"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
+    End Sub
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label14 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
 End Class
--- Lab08TravelExpenses/Lab08TravelExpenses/Form1.vb
+++ Lab08TravelExpenses/Lab08TravelExpenses/Form1.vb
@@ -1,3 +1,78 @@
 ﻿Public Class Form1
+    Function CalcMeals(ByVal Days As Integer)
+        Dim Reimbursed As Integer
+        Reimbursed = Days * 37
+        Return Reimbursed
+    End Function
 
+    Function CalcMileage(ByVal Miles As Integer)
+        Dim Reimbursed As Decimal
+        Reimbursed = Miles * 0.27
+        Return Reimbursed
+    End Function
+
+    Function CalcParking(ByVal Days As Integer)
+        Dim Reimbursed As Integer
+        Reimbursed = Days * 10
+        Return Reimbursed
+    End Function
+
+    Function CalcTaxi(ByVal Days As Integer)
+        Dim Reimbursed As Decimal
+        Reimbursed = Days * 20
+        Return Reimbursed
+    End Function
+
+    Function CalcLodging(ByVal Days As Integer)
+        Dim Reimbursed As Decimal
+        Reimbursed = Days * 95
+        Return Reimbursed
+    End Function
+
+    Function CalcPrivateVehicle(ByVal Miles As Integer)
+        Dim Reimbursed As Decimal
+        Reimbursed = Miles * 0.27
+        Return Reimbursed
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Days As Integer
+        Dim Miles As Decimal
+
+        Dim ReimbursedMeals As Integer
+        Dim ReimbursedMiles As Decimal
+        Dim ReimbursedParking As Integer
+        Dim ReimbursedLodging As Integer
+        Dim ReimbursedPrivate As Decimal
+
+        Dim CalcTotalReimbursement As Decimal
+        Dim CalcUnallowed As Decimal
+        Dim CalcSaved As Decimal
+
+        Days = CInt(TextBox3.Text)
+        ReimbursedMeals = CalcMeals(Days)
+
+        Miles = CInt(TextBox7.Text)
+        ReimbursedMiles = CalcMileage(Miles)
+
+        ReimbursedParking = CalcParking(Days)
+
+        ReimbursedLodging = CalcLodging(Days)
+
+        ReimbursedPrivate = CalcPrivateVehicle(Miles)
+
+        CalcTotalReimbursement = ReimbursedMeals + ReimbursedMiles + ReimbursedParking + ReimbursedLodging + ReimbursedPrivate
+
+        Label12.Text = "$" & CalcTotalReimbursement.ToString
+
+        CalcUnallowed = (TextBox5.Text / 37) + (TextBox8.Text / 10) + (TextBox9.Text / 20) + (TextBox2.Text / 95) + (TextBox7.Text / 0.27)
+
+        Label15.Text = "$" & CalcUnallowed.ToString
+
+        CalcSaved = CalcTotalReimbursement - CalcUnallowed
+
+        Label17.Text = "$" & CalcSaved.ToString
+
+
+    End Sub
 End Class
