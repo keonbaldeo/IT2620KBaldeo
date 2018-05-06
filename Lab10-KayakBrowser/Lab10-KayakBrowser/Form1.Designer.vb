0495b3a8fe55cb5f4b10ee9b3d1da8f96e4f8766
keonbaldeo <S00977574@acad.tri-c.edu>
2018-05-06 11:22:21 -0400

    Kayak Browser

---
+++ Lab10-KayakBrowser/Lab10-KayakBrowser/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"Recreational", "Racing", "Touring", "Surf", "White Water"})
+        Me.ListBox1.Location = New System.Drawing.Point(12, 50)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(227, 199)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(227, 23)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Select a Kayak Type to view a description"
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(245, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(339, 199)
+        Me.Label2.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(596, 261)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Lab10-KayakBrowser/Lab10-KayakBrowser/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        If ListBox1.SelectedIndex = 0 Then
+            Label2.Text = "This is a good Kayak for beginners, which is easy to use and usually inexpensive. It is usually not built for speed, but it is stable."
+        End If
+
+        If ListBox1.SelectedIndex = 1 Then
+            Label2.Text = "This is an excellent Kayak for experts. The level of difficulty is moderately high but it is very capable of reaching high speeds."
+        End If
+
+        If ListBox1.SelectedIndex = 2 Then
+            Label2.Text = " This is the perfect Kayak for people who are familiar with the sport. It is fairly easy to use."
+        End If
+
+        If ListBox1.SelectedIndex = 3 Then
+            Label2.Text = "This is a meduim difficulty Kayak. It takes some skill to use but is very rewarding in the end."
+        End If
+
+        If ListBox1.SelectedIndex = 4 Then
+            Label2.Text = "Pro Kayak-ers only! Dangerous but fun Kayaking experience."
+        End If
+    End Sub
+End Class
